ec46c33f5c71a5c486b3e081c62aa4087d71d0ed
Ethan Wang <ethanwang314159@gmail.com>
2025-10-01 16:50:17 +1000

    uwu

---
+++ utc_to_aest.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import pytz
+
+def UTCtoAEST(inp):
+    try:
+        AESTzone = pytz.timezone('Australia/Sydney')
+        UTC = datetime.strptime(inp, "%Y-%m-%dT%H:%M:%SZ")
+        UTC = pytz.utc.localize(UTC)
+        AEST = UTC.astimezone(AESTzone)
+        return AEST.strftime('%Y/%m/%d %H:%M:%S')
+    except Exception as e:
+        return f"Error: ${e}"
+
+if __name__ == "__main__":
+    inp = input("Input a UTC timestamp: ")
+    result = UTCtoAEST(inp)
+    print(result)
+else:
+    print('UTC to AEST module imported')
