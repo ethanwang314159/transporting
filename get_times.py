ef93794ba38925ddced618a3709c8bc135ec6033
Ethan Wang <ethanwang314159@gmail.com>
2025-10-01 17:29:57 +1000

    cleaning

--- get_times.py
+++ get_times.py
@@ -1,36 +1,49 @@
 import requests
 
-url = 'https://transportnsw.info/api/trip/v1/departure-list-request'
+def getTimes(time):
+    url = 'https://transportnsw.info/api/trip/v1/departure-list-request'
+    params = {
+        'date': '20251001',
+        'debug': 'false',
+        'depArrMacro': 'dep',
+        'depType': 'stopEvents',
+        'name': 'G2153190',
+        'time': time,
+        'type': 'stop',
+        'accessible': 'false'
+    }
 
-params = {
-    'date': '20251001',
-    'debug': 'false',
-    'depArrMacro': 'dep',
-    'depType': 'stopEvents',
-    'name': 'G2153190',
-    'time': '1500',
-    'type': 'stop',
-    'accessible': 'false'
-}
+    response = requests.get(url, params=params)
 
-response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        del data['version']
+        if 'locations' in data:
+            data['loc'] = {
+                'name':data['locations'][0]['name'],
+                'nick':data['locations'][0]['disassembledName'],
+                'coord':data['locations'][0]['coord'],
+                'suburb':data['locations'][0]['suburb'],
+            }
+            del data['locations']
+        else:
+            print('locations not found in the response.')
 
-if response.status_code == 200:
-    data = response.json()
-
-    if 'stopEvents' in data:
-        stop_events = data['stopEvents'][:15]
-        
-        for stop_event in stop_events:
-            unwanted_keys = ['isCancelled', 'isAccessible', 'transportation', 'location', 'id', 'alerts', 'isBookingRequired', 'onwardLocations', 'previousLocations', 'realtimeTripId', 'avmsTripId', 'isHighFrequency']
+        if 'stopEvents' in data:
+            stop_events = data['stopEvents'][:15]
             
-            for key in unwanted_keys:
-                if key in stop_event:
-                    del stop_event[key]
+            for stop_event in stop_events:
+                unwanted_keys = ['isCancelled', 'isAccessible', 'transportation', 'location', 'id', 'alerts', 'isBookingRequired', 'onwardLocations', 'previousLocations', 'realtimeTripId', 'avmsTripId', 'isHighFrequency']
+                
+                for key in unwanted_keys:
+                    if key in stop_event:
+                        del stop_event[key]
 
-        data['stopEvents'] = stop_events
-        print(str(data).replace("'", '"'))
+            data['stopEvents'] = stop_events
+            print(str(data).replace("'", '"'))
+        else:
+            print('stopEvents not found in the response.')
     else:
-        print('stopEvents not found in the response.')
-else:
-    print('Failed to retrieve data. Status code:', response.status_code)
+        print('Failed to retrieve data. Status code:', response.status_code)
+
+getTimes('1722')
